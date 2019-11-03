ff5fe7ed12aedeef2263b7bd5c3d3e4cdf9806b8
paulinevalero <paulinevalero@outlook.com>
2019-11-03 17:00:21 -0600

    add bulma

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <div class="background"></div>
     <header>
-      <h1>Random Useless Facts</h1>
+      <h1 class="title is-2 has-text-centered">Random Useless Facts</h1>
     </header>
     <main>
-      <button v-on:click="getUselessFact">Get a new useless fact</button>
+      <div class="columns is-centered is-mobile">
+        <div class="column is-narrow button-fact">
+          <button class="button is-success" v-on:click="getUselessFact">Get a new useless fact</button>
+        </div>
+      </div>
+
       <div>
-        <p>{{uselessFact.text}}</p>
+        <p class="title is-5 has-text-grey has-text-weight-light">{{uselessFact.text}}</p>
       </div>
     </main>
   </div>
@@ -36,4 +42,7 @@ export default {
 </script>
 
 <style>
+.button-fact {
+  padding: 30px;
+}
 </style>
